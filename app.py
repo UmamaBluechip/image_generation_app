af5f24840b66ae4395e6cd5bfccff86d9a276a27
UmamaBluechip <umamabluechiptech@gmail.com>
2024-04-18 11:16:52 +0530

    update

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import os
 import io
 import base64
-from langchain_together import Together
+#from langchain_together import Together
+from diffusers import StableDiffusionPipeline
+import torch
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+model_id = "CompVis/stable-diffusion-v1-4"
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-pipe = Together (
-    model="stabilityai/stable-diffusion-xl-base-1.0",
-    together_api_key="48515099b0ed4e22e56da54e50feb4adfaaa901a444b0c34bb33c66abe7b2c61"
-)
+pipe = StableDiffusionPipeline.from_pretrained(model_id)
+pipe = pipe.to(device)
+pipe.enable_attention_slicing()
+
+#pipe = Together (
+#    model="stabilityai/stable-diffusion-xl-base-1.0",
+#    together_api_key="48515099b0ed4e22e56da54e50feb4adfaaa901a444b0c34bb33c66abe7b2c61"
+#)
 
 @app.route('/', methods=['GET', 'POST'])
 def generate_image():
@@ -18,7 +26,7 @@ def generate_image():
         prompt = request.form['prompt']
 
         try:
-            image = pipe(prompt).images[0] 
+            image = pipe(prompt=prompt).images[0]
 
             buffered = io.BytesIO()
             image.save(buffered, format="PNG")
